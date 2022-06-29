76d37337dabcc14d4e0f30993f7fb3cd0c3946a8
Andy Lee <andy@e1337.co.uk>
2022-06-29 14:58:34 +0100

    .

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This is README.md
 
 Jump to: 
-* [Swagger](docs/api-docs/index.html)
+* [Swagger](docs/swagger/index.html)
 blah
 
 link to [page 1](/docs/page1.md)
--- docs/swagger/dist/swagger-initializer.js
+++ docs/swagger/dist/swagger-initializer.js
@@ -4,7 +4,7 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     url: "swagger.json",
-    dom_id: '#api-docs-ui',
+    dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
       SwaggerUIBundle.presets.apis,
